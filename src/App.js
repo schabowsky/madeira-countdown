f22264285bab23d37ae3e62aad12ac2aec8c6156
schabowsky <mpszymanek@gmail.com>
2020-06-07 14:49:39 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
+import Countdown from 'react-countdown';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dateInFuture = moment('2020-09-15 20:30:00', 'YYYY-MM-DD HH:mm:ss');
+    const renderer = ({days, hours, minutes, seconds, completed}) => {
+        if (completed) {
+            return <p>Time to go!</p>;
+        } else {
+            return <span>{days} days {hours}h {minutes}min {seconds}s</span>;
+        }
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={"https://www.wildrovertravel.com/wp-content/uploads/2019/02/Madeira-North-Coast.jpg"}
+                    alt="madeira"
+                    width={'50%'}
+                />
+                <p>Get ready for takeoff in:</p>
+                <Countdown date={dateInFuture} renderer={renderer}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
